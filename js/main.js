5b206fdcb02b420873d772703a768ec41544b900
yoshiki1995 <career.yoshiki23@gmail.com>
2019-10-20 18:18:52 +0900

    add probability

--- js/main.js
+++ js/main.js
@@ -4,11 +4,18 @@
   const btn = document.getElementById('btn')
 
   btn.addEventListener('click',()=>{
-    const results = ['大吉','中吉','凶','末吉'];
+    // const results = ['大吉','中吉','凶','末吉'];
     // const n = Math.floor(Math.random()* results.length);
     // btn.textContent = results[n];
-    btn.textContent = results[Math.floor(Math.random()* results.length)];
-
+    const n = Math.random();
+    if (n < 0.05){
+      btn.textContent = '大吉'; //5%
+    }else if(n < 0.2){
+      btn.textContent = '中吉';
+    }else{
+      btn.textContent = '凶';
+    }
+  
     // btn.textContent = n;
 
     // switch(n){
